73e22bf4496e466ffa97fc1cdd4334e0d2e0cd14
Hassan97-js <hassan.sweden.dev@gmail.com>
2023-09-13 15:47:53 +0200

    protect all routes with next auth js

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,6 @@ export { default } from "next-auth/middleware";
 
 // Applies next-auth only to matching routes - can be regex
 // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = {
-  matcher: ["/profile", "/create-prompt", "/auth", "/update-prompt", "/api"]
-};
+// export const config = {
+//   matcher: ["/profile", "/create-prompt", "/auth", "/update-prompt", "/api"]
+// };
